305af353a26a9b7b44b81244a0fd994b91fd22c1
BeomyeonAndrewKim <beom911@gmail.com>
2017-11-17 17:03:28 +0900

    로그인 폼 관련 영역 일부 마크업 완성

--- js/menu-tab.js
+++ js/menu-tab.js
@@ -1,15 +1,31 @@
-$(document).ready(function(){
-	var menu = $('.main-menu > li');
-	var subMenu = $('.sub-menu');
-	var last = $('.sub-menu li:last-child a');
-	
-	menu.hover(function(){
-	$(this).find('.sub-menu').toggleClass('menu-act');
-	});
-	menu.focus(function(){
-		$(this).find('.sub-menu').addClass('menu-act');
-		});
-	last.focusout(function(){
-		$(this).parents('.sub-menu').removeClass('menu-act');
-	});
+// $(document).ready(function(){
+// 	var menu = $('.main-menu > li');
+// 	var subMenu = $('.sub-menu');
+// 	var last = $('.sub-menu li:last-child a');
+
+// 	menu.hover(function(){
+// 	$(this).find('.sub-menu').toggleClass('menu-act');
+// 	});
+// 	menu.focus(function(){
+// 		$(this).find('.sub-menu').addClass('menu-act');
+// 		});
+// 	last.focusout(function(){
+// 		$(this).parents('.sub-menu').removeClass('menu-act');
+// 	});
+// });
+
+$(document).ready(function() {
+  var menu = $('.main-menu>li');
+  // var box = $('.box');
+
+  menu.hover(function() {
+    $(this).find('.sub-menu').toggleClass('menu-act')
+  });
+  menu.focusin(function() {
+    $(this).siblings().find('.sub-menu').removeClass('menu-act');
+    $(this).find('.sub-menu').addClass('menu-act');
+  });
+  // box.click(function() {
+  //   $(this).toggleClass('box-act');
+  // });
 });
